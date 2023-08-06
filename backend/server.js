2179b27d66995c8c050e447b0b51839c08a08e26
David Stefank <113402850+david-stefanik@users.noreply.github.com>
2023-08-06 07:26:59 -0700

    server.js small fix in the frront end reciving code

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,13 @@ app.use(express.json())
 const mongoose=require (`mongoose`)
 
 
+app.use('/api',router);
+app.use(bodyParser.json());
+
+app.post('/api/cars',(req,res)=>{
+  const carData=req.body
+});
+
 mongoose.connect(process.env.MONGO_URI,{
   useNewUrlParser:true,
   useUnifiedTopology:true
@@ -15,4 +22,3 @@ mongoose.connect(process.env.MONGO_URI,{
 app.listen(27016,function(){
     console.log("express server is running on port 27017")
   })
- app.use('/api',carRoutes);
